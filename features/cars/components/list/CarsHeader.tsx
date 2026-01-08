8a40a61002df6f8f628aadb5ca467afa1d55c525
emanuelpps <emanuelpages.ps@gmail.com>
2026-01-08 00:31:55 -0300

    feat: add CarsHeader component and adjust main page structure

---
+++ features/cars/components/list/CarsHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarsHeader: React.FC = () => {
+    return (
+        <header className="cars-header">
+            <h1>Descubrí todos los modelos</h1>
+        </header>
+    );
+};
+
+export default CarsHeader;
