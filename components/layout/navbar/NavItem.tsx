2f3dd1794c7f8670dc936e8afaf127a5b2415b11
emanuelpps <emanuelpages.ps@gmail.com>
2026-01-07 23:08:05 -0300

    style: added width correction in NavItem

--- components/layout/navbar/NavItem.tsx
+++ components/layout/navbar/NavItem.tsx
@@ -10,13 +10,13 @@ export function NavItem({ href, active = false, children }: NavItemProps) {
   return (
     <Link
       href={href}
-      className={`relative pb-1 text-sm font-medium transition-colors pr-5 pl-5 font-semibold ${
+      className={`min-w-[126px] flex justify-center relative pb-1 text-sm font-medium transition-colors font-semibold ${
         active ? "text-red-600" : "text-neutral-800"
       }`}
     >
       {children}
       {active && (
-        <span className="absolute left-0 -bottom-[23px] h-[2px] w-full bg-red-600" />
+        <span className="absolute left-0 -bottom-[23px] h-[4px] w-full bg-red-600" />
       )}
     </Link>
   );
