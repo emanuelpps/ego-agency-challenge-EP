1b1f0baa069afae9a6c1e9a114387264cd9c3b93
emanuelpps <emanuelpages.ps@gmail.com>
2026-01-09 10:34:06 -0300

    feat: refine PrimaryButton styles by removing padding and improving text legibility

--- components/ui/buttons/Buttons.tsx
+++ components/ui/buttons/Buttons.tsx
@@ -3,11 +3,11 @@ import { ButtonProps } from "./button.types";
 
 export const PrimaryButton: React.FC<ButtonProps> = ({ label, onClick }) => (
   <button
-    className="flex min-w-[152px] h-[34px] rounded-[40px] bg-[#191919] text-[#FFFFFF] text-[13px] font-semibold hover:bg-[#333333] transition-all duration-300 cursor-pointer justify-center items-center border border-[#191919] px-8 py-5"
+    className="flex min-w-[152px] h-[34px] rounded-[40px] bg-[#191919] hover:bg-[#333333] transition-all duration-300 cursor-pointer justify-center items-center border border-[#191919]"
     aria-label={typeof label === "string" ? label : "primary button"}
     onClick={onClick}
   >
-    <div className="flex w-full justify-center items-center gap-2 text-xs lg:text-lg">
+    <div className="flex w-full justify-center items-center text-[#FFFFFF] text-[13px] font-semibold">
       {label}
     </div>
   </button>
